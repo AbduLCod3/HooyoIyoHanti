d791b7b4dc78274a9cbdee3ac0bcef9c7013171f
Abdul <46938888+AbduLCod3@users.noreply.github.com>
2025-11-08 10:27:04 -0600

    Build layout, basic header and nav links

---
+++ app/_components/Header.jsx
@@ -0,0 +1,63 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
+
+function Header() {
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path);
+  }, []);
+  return (
+    <div className="p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white">
+      <div className="flex gap-12 items-center">
+        <Link href={"/"}>
+          <Image src={"/logo3.svg"} width={150} height={150} alt="logo"/>{" "}
+        </Link>
+        <ul className="hidden md:flex gap-10 ">
+          <Link href={"/buy"}>
+            <li
+              className={`'hover:text-primary font-medium text-sm cursor-pointer' ${
+                path == "buy" && "text-primary"
+              }`}
+            >
+              Buy a Home
+            </li>
+          </Link>
+          <Link href={"/rent"}>
+            <li
+              className={`'hover:text-primary font-medium text-sm cursor-pointer' ${
+                path == "rent" && "text-primary"
+              }`}
+            >
+              Rent a Home
+            </li>
+          </Link>
+          <Link href={"/"}>
+            <li
+              className={`'hover:text-primary font-medium text-sm cursor-pointer' ${
+                path == "find" && "text-primary"
+              }`}
+            >
+              Find an Agent
+            </li>
+          </Link>
+        </ul>
+      </div>
+
+      <div className="flex gap-2">
+        <Button className="flex gap-2">
+          <Plus className="h-5 w-5" />
+          Post Your Listing
+        </Button>
+        <Button variant="outline">Login</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
